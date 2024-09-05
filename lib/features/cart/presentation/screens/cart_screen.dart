4f33eacc0c984790a35ffab0844701d24b6efbf2
Vitthal <verma.vitthal2004@gmail.com>
2024-09-06 00:02:51 +0530

    add delete item slidable feature

--- lib/features/cart/presentation/screens/cart_screen.dart
+++ lib/features/cart/presentation/screens/cart_screen.dart
@@ -2,6 +2,7 @@ import 'package:diploy_assignment/features/cart/presentation/bloc/cart_bloc.dart
 import 'package:diploy_assignment/features/cart/presentation/widgets/payment_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 
 class CartScreen extends StatelessWidget {
@@ -36,31 +37,51 @@ class CartScreen extends StatelessWidget {
                 final product = products.keys.elementAt(index);
                 final quantity = products[product] ?? 1;
 
-                return ListTile(
-                  leading: Image.asset(product.imageUrl, width: 50, height: 50),
-                  title: Text(product.name),
-                  subtitle: Text('\$${product.price * quantity}'),
-                  trailing: Row(
-                    mainAxisSize: MainAxisSize.min,
+                return Slidable(
+                  key: ValueKey(product.id),
+                  endActionPane: ActionPane(
+                    motion: const ScrollMotion(),
                     children: [
-                      IconButton(
-                        icon: const Icon(Icons.remove),
-                        onPressed: () {
+                      SlidableAction(
+                        onPressed: (context) {
                           context
                               .read<CartBloc>()
-                              .add(DecreaseQuantityEvent(product));
+                              .add(DeleteFromCartEvent(product: product));
                         },
-                      ),
-                      Text('$quantity', style: TextStyle(fontSize: 15.sp)),
-                      IconButton(
-                        icon: const Icon(Icons.add),
-                        onPressed: () {
-                          context
-                              .read<CartBloc>()
-                              .add(IncreaseQuantityEvent(product));
-                        },
+                        backgroundColor: Colors.red,
+                        foregroundColor: Colors.white,
+                        icon: Icons.delete,
+                        label: 'Delete',
                       ),
                     ],
+                  ),
+                  child: ListTile(
+                    leading:
+                        Image.asset(product.imageUrl, width: 50, height: 50),
+                    title: Text(product.name),
+                    subtitle: Text('\$${product.price * quantity}'),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        IconButton(
+                          icon: const Icon(Icons.remove),
+                          onPressed: () {
+                            context
+                                .read<CartBloc>()
+                                .add(DecreaseQuantityEvent(product));
+                          },
+                        ),
+                        Text('$quantity', style: TextStyle(fontSize: 15.sp)),
+                        IconButton(
+                          icon: const Icon(Icons.add),
+                          onPressed: () {
+                            context
+                                .read<CartBloc>()
+                                .add(IncreaseQuantityEvent(product));
+                          },
+                        ),
+                      ],
+                    ),
                   ),
                 );
               },
